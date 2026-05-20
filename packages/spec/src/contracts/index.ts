b2d0c0ecd86369ab651e484048dd8c033b9e0cd4
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2026-05-20 23:08:57 +0800

    Update index.ts

--- packages/spec/src/contracts/index.ts
+++ packages/spec/src/contracts/index.ts
@@ -49,3 +49,4 @@ export * from './deploy-pipeline-service.js';
 export * from './tenant-router.js';
 export * from './app-lifecycle-service.js';
 export * from './seed-loader-service.js';
+export * from './crypto-provider.js';
